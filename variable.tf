d820a5270fe65ab0d8c0655d33f79bac402962eb
florient2016 <koffieza71@gmail.com>
2023-09-19 12:07:49 +0200

    Terraform code to provision ceph cluster

--- variable.tf
+++ variable.tf
@@ -1,10 +1,26 @@
 #**********
 #Pré-requisite:
-#   - Make sure to update default "AwsRegion" and Availability Zone depend your preference Region
+# This file is a representation of config file
+# Field free to update value and parameter
+#
+#   - Make sure to update default "AwsRegion" and "Availability Zone" value
+#             For my testing purpose, i used
+#             eu-west-3   ==> Paris region
+#             eu-west-3b  ==> corresponding AZ
+#   - Ami is not the same depend on your region (Update value accordinly)
+#              All operating system based on latest Red Hat OS
+#              ami-0d767e966f3458eb5 ==> Reh Hat ami ID on eu-west-3
+#   - SSH path location
+#               Based on windows operating system my path set to:
+#               ~/.ssh/id_rsa.pub   ==> ssh public key for ec2 instance connection
+#   - Instance type can be also change
+#               t3.medium ==> MasterNode instance type
+#           /!\ Only master node will be cost, all client, grafana and node used t2.micro instance type
 #**********
+
 variable "AwsRegion" {
   default = "eu-west-3"
-  description = "Default AWS region based in Virginia"
+  description = "Default AWS region based in Paris"
 }
 
 variable "AvailabilityZone" {
